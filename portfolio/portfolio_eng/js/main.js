958f2e90c0351a006fd7d6ca33f64ffe38179140
VsevolodKarpykov <64524830+VsevolodKarpykov@users.noreply.github.com>
2022-08-05 13:07:35 +0300

    Add files via upload

---
+++ portfolio/portfolio_eng/js/main.js
@@ -0,0 +1,18 @@
+var square = document.querySelectorAll('figure');
+
+
+
+
+for (var i = 0; i < square.length; i++) {
+  square[i].addEventListener('mousemove', function(e){
+  var x = e.offsetX;
+  var y = e.offsetY;
+
+  this.style.background = 'radial-gradient(circle at ' + x + 'px ' + y + 'px, #fff5 1%, rgba(100, 100, 100, .15) 60%)';
+
+});
+
+square[i].addEventListener('mouseout', function(e){
+  this.style.background = 'radial-gradient(circle at 122px 100px, rgba(100, 100, 100, .15) 1%, rgba(100, 100, 100, .15) 70%)';
+});
+}
