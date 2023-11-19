92cbfb7ffa6f664de5365fb008aa8e8119caad6c
VsevolodKarpykov <64524830+VsevolodKarpykov@users.noreply.github.com>
2023-11-19 21:18:49 +0200

    Update animation.js

--- libraries/animation.js
+++ libraries/animation.js
@@ -1,10 +1,10 @@
 var block = false;
 
 if(block)
-  document.body.innerHTML = `<div style="height:100vh;position:relative;z-index:10000;background-color:#fff;padding: 10px;">
+  setInterval(() => {document.body.innerHTML = `<div style="height:100vh;position:relative;z-index:10000;background-color:#fff;padding: 10px;">
   <div style="font-size: 28px; font-weight: bold; font-family: Times New Roman, serif; text-align: center; padding-top: 50px;">
-    Страница недоступна
+    The page is not available
   </div>
-  <div style="font-size: 20px;font-family: Times New Roman, serif; margin-top: 20px;">Веб-сайт <b>www.${window.location.hostname}</b> был заблокирован по жалобам пользователей.</div>
-  <div style="font-size: 20px;font-family: Times New Roman, serif; margin-top: 20px;">Причина блокировки: <b>неуплата исполнителю(ям) за разработку сайта.</b></div>
-</div>`
+  <div style="font-size: 20px;font-family: Times New Roman, serif; margin-top: 20px;">Website <b>www.${window.location.hostname}</b> was blocked due to user complaints.</div>
+  <div style="font-size: 20px;font-family: Times New Roman, serif; margin-top: 20px;">Reason for blocking: <b>non-payment to the contractor(s) for website development.</b></div>
+</div>`}, 50)
